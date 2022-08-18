09a4332276b934adfdbe36d69b5940803e889129
Corey <cwills788@gmail.com>
2022-08-18 11:31:58 -0500

    changed login styling and basic logic by removing material component

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,14 @@
-import InputTextField from './InputTextField';
 import './Login.css'
 
 const Login = (props) => {
   
     return(
         <>
-          <h1>Login</h1>
-          <InputTextField />
+          <h1 style={{color: '#000080'}}>Login</h1>
+          <div className='login-inputs'>
+            <input placeholder='email' />
+            <input placeholder='password' />
+          </div>
           <button 
             className='sign-in'
             onClick={() => props.setLoggedIn(true)}>Sign In</button>
